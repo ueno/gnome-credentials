acc99eb12921727205b56ee7dd2331b2b21dd3ad
Daiki Ueno <dueno@src.gnome.org>
2016-02-01 17:01:30 +0900

    autogen.sh: Change from `foo' to 'foo'

--- autogen.sh
+++ autogen.sh
@@ -8,16 +8,16 @@ olddir=`pwd`
 cd "$srcdir"
 
 (test -f configure.ac) || {
-        echo "*** ERROR: Directory \`$srcdir' does not look like the top-level project directory ***"
+        echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
         exit 1
 }
 
 PKG_NAME=`autoconf --trace 'AC_INIT:$1' configure.ac`
 
 if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
-        echo "*** WARNING: I am going to run \`configure' with no arguments." >&2
+        echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
         echo "*** If you wish to pass any to it, please specify them on the" >&2
-        echo "*** \`$0\' command line." >&2
+        echo "*** '$0' command line." >&2
         echo "" >&2
 fi
 
@@ -32,7 +32,7 @@ if [ "$NOCONFIGURE" = "" ]; then
         "$srcdir/configure" "$@" || exit 1
 
         if [ "$1" = "--help" ]; then exit 0 else
-                echo "Now type \`make' to compile $PKG_NAME" || exit 1
+                echo "Now type 'make' to compile $PKG_NAME" || exit 1
         fi
 else
         echo "Skipping configure process."
